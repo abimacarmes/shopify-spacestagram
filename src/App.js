c8899925e03e200774b0b5c4e5d3ad7d53bf4fe8
Abigail M-A <70247056+abimacarmes@users.noreply.github.com>
2022-01-18 16:26:17 -0500

    add styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
 import Image from './Image.js';
 import './App.css';
 
@@ -13,7 +12,7 @@ export default class App extends Component {
   }
 
   fetchApiResults = () => {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=STXM8eM5nh0gYoIGwu9LBHAJUXdJ6HsO6ipuiL62&count=10')
+    fetch('https://api.nasa.gov/planetary/apod?api_key=STXM8eM5nh0gYoIGwu9LBHAJUXdJ6HsO6ipuiL62&count=5')
       .then(response => response.json())
       .then(responseJson => {
           let prevResults = this.state.apiResults;
@@ -25,24 +24,17 @@ export default class App extends Component {
       })
       .catch(error => alert(error.message));
   }
-
-  /*let scrollObserver = new IntersectionObserver(this.fetchApiResults(), {threshold: 0.9});
-  let target = document.querySelector('.images-container');
-
-  scrollObserver.observe(target);*/
   
   render(){
     
 
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Spacestagram</h1>
-          <h2>Your go-to source of NASA images - peruse at your leisure!</h2>
-          <h4>Built by Abigail MacKenzie-Armes</h4>
-        </header>
         <div className='images-container'>
           {this.state.apiResults.map(image => <Image id={this.state.apiResults.indexOf(image)} image={image}/>)}
+        </div>
+        <div className='load'>
+          <button className='load-button' onClick={() => this.fetchApiResults()} >Load More Images</button>
         </div>
       </div>
     )
--- src/Image.js
+++ src/Image.js
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 
 function Image(props) {
     let {image} = props;
+    let {key} = props;
     const [liked, setLiked] = useState(false);
         
     return (
-      <div className="image">
-        <img src={image.hdurl}/>
-        <h3>{image.title}</h3>
-        <h4>{image.date}</h4>
-        <p>{image.explanation}</p>
-        <button className={liked ? 'liked' : ''} onClick={() => setLiked(!liked)}>
-            {liked ? 'Liked' : 'Like'}
-        </button>
+      <div key={key} className='card'>
+        <img className='image' src={image.hdurl}/>
+        <div className='image-text'>
+            <h3>{image.title}</h3>
+            <p>{image.explanation}</p>
+            <h4>Date: {image.date}</h4>
+            <button className={liked ? 'liked' : ''} onClick={() => setLiked(!liked)}>
+                {liked ? 'Unlike' : 'Like'}
+            </button>
+        </div>
       </div>
     );
   }
