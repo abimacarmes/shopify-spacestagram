145ccfeff8d1b4f90c8fbf34f7af6d38304ac654
Abigail M-A <70247056+abimacarmes@users.noreply.github.com>
2022-01-18 20:00:24 -0500

    added share functionality & additional styling

--- src/App.js
+++ src/App.js
@@ -25,11 +25,30 @@ export default class App extends Component {
       .catch(error => alert(error.message));
   }
   
+  fetchApiResultsDate = () => {
+    let date = document.getElementById('date-input').value;    
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=STXM8eM5nh0gYoIGwu9LBHAJUXdJ6HsO6ipuiL62&date=${date}`)
+      .then(response => response.json())
+      .then(responseJson => {
+        let newResults = [];
+        newResults.push(responseJson);
+        this.setState({
+            apiResults : newResults
+          })
+      })
+      .catch(error => alert(error.message));
+  }
+
   render(){
-    
+    let maxDate = new Date().toISOString().split('T')[0];
 
     return (
       <div className="App">
+        <div className='date-filter'>
+          <h3>Get specific Picture of the Day:</h3>
+          <input id='date-input' type='date' name='date' min='2015-01-01' max={maxDate} onChange={() => this.fetchApiResultsDate()}></input>
+          <button className='reset' onClick={() => this.fetchApiResults()}>Reset</button>
+        </div>
         <div className='images-container'>
           {this.state.apiResults.map(image => <Image id={this.state.apiResults.indexOf(image)} image={image}/>)}
         </div>
--- src/Image.js
+++ src/Image.js
@@ -4,7 +4,8 @@ function Image(props) {
     let {image} = props;
     let {key} = props;
     const [liked, setLiked] = useState(false);
-        
+    const [share, setShare] = useState(false);
+    //<a href={image.hdurl} className={share ? 'share-link visible' : 'share-link'} onClick={() => window.open(image.hdurl)}>{image.hdurl}</a>    
     return (
       <div key={key} className='card'>
         <img className='image' src={image.hdurl}/>
@@ -12,9 +13,19 @@ function Image(props) {
             <h3>{image.title}</h3>
             <p>{image.explanation}</p>
             <h4>Date: {image.date}</h4>
-            <button className={liked ? 'liked' : ''} onClick={() => setLiked(!liked)}>
-                {liked ? 'Unlike' : 'Like'}
-            </button>
+            <div className='image-buttons'>   
+                <button className={liked ? 'button-pressed' : ''} onClick={() => setLiked(!liked)}>
+                    {liked ? 'Unlike' : 'Like'}
+                </button>
+                <button className={share?'button-pressed button-share':'button-share'} onClick={() => setShare(!share)}>
+                    Share Image
+                </button>
+                <a className={share ? '' :'hidden'} href="www.google.com" 
+                    onClick={(event) => {
+                        event.preventDefault();
+                        window.open(image.hdurl);
+                }}>{image.hdurl}</a>
+            </div>
         </div>
       </div>
     );
